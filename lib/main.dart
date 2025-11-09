6c7fdf47c25d14ae7f876331203406d6758d9efc
zainalsalamun <zainalrtf@gmail.com>
2025-11-10 05:42:54 +0700

    update : main with splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:monimate/data/controller/theme_controller.dart';
-import 'package:monimate/data/controller/transaction_controller.dart';
-import 'package:monimate/data/services/hive_service.dart';
-import 'package:monimate/pages/shell.dart';
+import 'package:monimate/pages/splash_page.dart';
 import 'theme/app_theme.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await GetStorage.init();
+
+//   //Init Hive & Controller
+//   await HiveService.init();
+//   Get.put(TransactionController());
+//   Get.put(ThemeController());
+
+//   runApp(const MoniMateApp());
+// }
+
+// class MoniMateApp extends StatelessWidget {
+//   const MoniMateApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'MoniMate',
+//       theme: AppTheme.light(),
+//       darkTheme: AppTheme.dark(),
+//       themeMode: ThemeMode.system,
+//       home: const Shell(),
+//     );
+//   }
+// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
 
-  //Init Hive & Controller
-  await HiveService.init();
-  Get.put(TransactionController());
-  Get.put(ThemeController());
-
+  // Hive init dipindah ke Splash agar tampil splash saat loading
   runApp(const MoniMateApp());
 }
 
@@ -30,7 +51,7 @@ class MoniMateApp extends StatelessWidget {
       theme: AppTheme.light(),
       darkTheme: AppTheme.dark(),
       themeMode: ThemeMode.system,
-      home: const Shell(),
+      home: const SplashPage(),
     );
   }
 }
