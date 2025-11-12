c532a7c05cd08a903fc83b00304112aed4b959d6
zainalsalamun <zainalrtf@gmail.com>
2025-11-12 17:05:57 +0700

    add : format currency

---
+++ lib/utils/format_currency.dart
@@ -0,0 +1,12 @@
+import 'package:intl/intl.dart';
+
+class CurrencyFormat {
+  static String format(double amount) {
+    final formatCurrency = NumberFormat.currency(
+      locale: 'id_ID',
+      symbol: 'Rp ',
+      decimalDigits: 0,
+    );
+    return formatCurrency.format(amount);
+  }
+}
