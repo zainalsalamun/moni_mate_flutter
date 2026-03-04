eb74238a6009d94c1fe3a1210e052e0d775f1442
zainalsalamun <zainalrtf@gmail.com>
2026-03-04 13:52:27 +0700

    feat: introduce recurring transactions functionality with dedicated Hive service and UI in settings.

--- lib/data/services/hive_service.dart
+++ lib/data/services/hive_service.dart
@@ -1,22 +1,28 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import '../models/transaction_model.dart';
 import '../models/category_model.dart';
+import '../models/recurring_transaction_model.dart';
 
 class HiveService {
   static const String boxName = 'transactions';
   static const String categoryBoxName = 'categories';
+  static const String recurringBoxName = 'recurring_transactions';
 
   static Future<void> init() async {
     await Hive.initFlutter();
     Hive.registerAdapter(TransactionModelAdapter());
     Hive.registerAdapter(CategoryModelAdapter());
+    Hive.registerAdapter(RecurringTransactionModelAdapter());
     await Hive.openBox<TransactionModel>(boxName);
     await Hive.openBox<CategoryModel>(categoryBoxName);
+    await Hive.openBox<RecurringTransactionModel>(recurringBoxName);
   }
 
   static Box<TransactionModel> get box => Hive.box<TransactionModel>(boxName);
   static Box<CategoryModel> get categoryBox =>
       Hive.box<CategoryModel>(categoryBoxName);
+  static Box<RecurringTransactionModel> get recurringBox =>
+      Hive.box<RecurringTransactionModel>(recurringBoxName);
 
   static Future<void> addTransaction(TransactionModel tx) async {
     await box.put(tx.id, tx);
@@ -45,5 +51,19 @@ class HiveService {
 
   static Future<void> deleteCategory(String id) async {
     await categoryBox.delete(id);
+  }
+
+  // Recurring Transactions Functionality
+  static Future<void> addRecurringTransaction(
+      RecurringTransactionModel tx) async {
+    await recurringBox.put(tx.id, tx);
+  }
+
+  static List<RecurringTransactionModel> getAllRecurring() {
+    return recurringBox.values.toList();
+  }
+
+  static Future<void> deleteRecurringTransaction(String id) async {
+    await recurringBox.delete(id);
   }
 }
